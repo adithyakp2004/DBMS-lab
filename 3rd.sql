5a99400562115902bdd7bc6c57bdb7462dfbb6b8
adithyakp2004 <45890377+adithyakp2004@users.noreply.github.com>
2026-06-25 14:06:16 +0530

    Update 3rd.sql

--- 3rd.sql
+++ 3rd.sql
@@ -1,3 +1,27 @@
+create table team (tid int primary key, tname varchar(20), coach
+varchar(20), captain_pid int, city varchar(20));
+
+create table player (pid int primary key, pname varchar(20),
+age int, tid int references team(tid));
+
+create table stadium (sid int primary key, sname varchar(20),
+picode number(8), city varchar(20), area varchar(20));
+
+create table match (mid int primary key, mdate date,
+time varchar(6),
+sid int references stadium(sid),
+team1_id int references team(tid),
+team2_id int references team(tid),
+winning_team_id int references team(tid),
+man_of_match int references player(pid));
+
+create table player_phone
+(pid int references
+player(pid), phone int,
+primary
+key(pid,phone));
+
+
 select pname,tname,age from player p,team t
 where p.tid=t.tid and age=(select min(age) from player);
 
