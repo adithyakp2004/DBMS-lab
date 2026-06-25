c1f4dff02b978e309bf867b7849cfdd7e68147ef
adithyakp2004 <45890377+adithyakp2004@users.noreply.github.com>
2026-06-25 10:28:44 +0530

    Remove line numbers from SQL statements

--- 2nd.sql
+++ 2nd.sql
@@ -1,16 +1,16 @@
-1. update studentr set total=mark1+mark2+mark3;
+update studentr set total=mark1+mark2+mark3;
 
-2. update studentr set gpa=total/3;
-   select * from studentr;
+update studentr set gpa=total/3;
+select * from studentr;
 
-3. select * from studentr where date_of_birth like '%02';
+select * from studentr where date_of_birth like '%02';
 
-4. select * from studentr where branch='MCA';
+select * from studentr where branch='MCA';
 
-5. select branch,max(gpa) from studentr group by branch;
+select branch,max(gpa) from studentr group by branch;
 
-6. select * from studentr where sname like 'S%';
+select * from studentr where sname like 'S%';
 
-7. select * from studentr where sname like '%AR';
+select * from studentr where sname like '%AR';
 
-8. delete from studentr where usn='1RN23MC047';
+delete from studentr where usn='1RN23MC047';
