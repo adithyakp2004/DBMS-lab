ec0a93a5fa894650b9ff8df84d5f3445288bb3fd
adithyakp2004 <45890377+adithyakp2004@users.noreply.github.com>
2026-08-02 20:16:58 +0530

    Refactor SQL table creation and insert statements

--- 3rd.sql
+++ 3rd.sql
@@ -1,26 +1,42 @@
 create table team (tid int primary key, tname varchar(20), coach
 varchar(20), captain_pid int, city varchar(20));
+INSERT INTO team VALUES (123, 'rcb', 'sunil', 1, 'bangalore');
+INSERT INTO team VALUES (124, 'csk', 'laxman', 3, 'chennai');
+INSERT INTO team VALUES (125, 'royals', 'singh', 4, 'rajasthan');
+INSERT INTO team VALUES (126, 'daredevils', 'sehwag', 2, 'delhi');
+INSERT INTO team VALUES (127, 'kkr', 'ravi', 5, 'goa');
 
 create table player (pid int primary key, pname varchar(20),
 age int, tid int references team(tid));
+INSERT INTO player VALUES (1, 'sachin', 33, 123);
+INSERT INTO player VALUES (2, 'dravid', 32, 124);
+INSERT INTO player VALUES (3, 'dhoni', 30, 124);
+INSERT INTO player VALUES (4, 'raina', 30, 125);
+INSERT INTO player VALUES (5, 'kohli', 23, 126);
 
 create table stadium (sid int primary key, sname varchar(20),
 picode number(8), city varchar(20), area varchar(20));
+INSERT INTO stadium VALUES (111, 'chinnaswamy', 56001, 'bangalore', 'mg road');
+INSERT INTO stadium VALUES (222, 'kotla', 460009, 'delhi', 'highway');
+INSERT INTO stadium VALUES (333, 'international', 38883, 'chennai', 'trnagar');
+INSERT INTO stadium VALUES (444, 'ksca', 560098, 'bangalore', 'peenya');
+INSERT INTO stadium VALUES (555, 'csca', 567772, 'cochin', 'beach road');
 
-create table match (mid int primary key, mdate date,
-time varchar(6),
-sid int references stadium(sid),
-team1_id int references team(tid),
-team2_id int references team(tid),
-winning_team_id int references team(tid),
-man_of_match int references player(pid));
+create table match (mid int primary key, mdate date,time varchar(6),sid int references stadium(sid),
+team1_id int references team(tid),team2_id int references team(tid),
+winning_team_id int references team(tid),man_of_match int references player(pid));
+INSERT INTO match VALUES (101, '10-JAN-17', '10am', 111, 123, 124, 123, 1);
+INSERT INTO match VALUES (102, '11-JAN-17', '9am', 222, 124, 126, 126, 5);
+INSERT INTO match VALUES (103, '12-JAN-17', '11am', 111, 125, 126, 126, 5);
+INSERT INTO match VALUES (104, '10-JAN-17', '12am', 111, 125, 123, 123, 1);
+INSERT INTO match VALUES (105, '11-JAN-17', '9am', 222, 124, 126, 126, 5);
 
-create table player_phone
-(pid int references
-player(pid), phone int,
-primary
-key(pid,phone));
-
+create table player_phone(pid int references player(pid), phone int,
+primary key(pid,phone));
+INSERT INTO player_phone VALUES (1, 998882928);
+INSERT INTO player_phone VALUES (2, 877563733);
+INSERT INTO player_phone VALUES (2, 988928822);
+INSERT INTO player_phone VALUES (2, 877366383);
 
 select pname,tname,age from player p,team t
 where p.tid=t.tid and age=(select min(age) from player);
